98a5fbb7fb5797b30907fdca48f3dd2dc7b6fcf6
Seth Bowman <brandonsethbowman@gmail.com>
2023-07-20 11:50:15 -0500

    Add project files.

---
+++ VariablesExample/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace VariablesExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables
+
+            //Number Types
+            //Declaring a variable (creation)
+            int wholeNumber;
+            //Initialize a variable (give it value)
+            wholeNumber = 100;
+
+            //Or do both
+            int otherNumber = 100;
+
+            double avg;
+            avg = 98.5;
+
+            decimal money = 500.00m;
+
+            //Word types
+            string myName = "Seth Bowman";
+
+            char key = '1';
+
+            //True/False
+            bool isCoding = false;
+
+
+            //String Interpolation
+            Console.WriteLine($"My name is {myName}. The number {wholeNumber} is a whole number and {avg} is not.");
+
+
+        }
+    }
+}
